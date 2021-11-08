c643f14e70975281686e4efa92a75a9aa9e45a65
Samarit <kislyakaleksey@gmail.com>
2021-11-08 23:49:07 +0300

    new fonts, newicons, form style

--- src/main.js
+++ src/main.js
@@ -8,30 +8,30 @@ const inputName = document.getElementById('inputName')
 const inputEmail = document.getElementById('inputEmail')
 const inputPhone = document.getElementById('inputPhone')
 
-let maskPhoneOptions = {
-  mask: '+{7}(000)000-00-00',
-  lazy: false
-}
-let maskPhone = IMask(inputPhone, maskPhoneOptions)
+//let maskPhoneOptions = {
+//  mask: '+{7}(000)000-00-00',
+//  lazy: false
+//}
+//let maskPhone = IMask(inputPhone, maskPhoneOptions)
 
-modalForm.onsubmit = (e) => {
-  e.preventDefault()
-  modalForm.classList.add('was-validated')
-
-  let errors = formValidate(modalForm)
-  console.log(errors)
-  
-  let formData = new FormData(modalForm)
-  formData.set('phone', maskPhone.unmaskedValue)
-
-  if (!errors) {
-    fetch('/api/sendmail', {
-      method: 'POST',
-      body: formData
-    })
-  }
-  
-}
+//modalForm.onsubmit = (e) => {
+//  e.preventDefault()
+//  modalForm.classList.add('was-validated')
+//
+//  let errors = formValidate(modalForm)
+//  console.log(errors)
+//  
+//  let formData = new FormData(modalForm)
+//  formData.set('phone', maskPhone.unmaskedValue)
+//
+//  if (!errors) {
+//    fetch('/api/sendmail', {
+//      method: 'POST',
+//      body: formData
+//    })
+//  }
+//  
+//}
 
 
 
