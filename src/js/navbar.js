972ce3b6896f956c01b97062b01af7cb9e3b48b4
Samarit <kislyakaleksey@gmail.com>
2021-11-14 15:59:09 +0300

    Navbar animation removed

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -3,24 +3,13 @@ const navbarToggler = document.getElementById('navbar-toggler')
 const ANIMATION_SPEED = 300
 
 function openMobileNavbar() {
-  navbar.classList.add('opening')
-  setTimeout(() => {
-    navbar.classList.remove('opening')
-    navbar.classList.add('opened')
-  }, ANIMATION_SPEED)
+  navbar.classList.add('opened')
 }
 
 function closeMobileNavbar() {
   navbar.classList.remove('opened')
-  navbar.classList.add('closing')
-  setTimeout(() => {
-    navbar.classList.remove('closing')
-  }, ANIMATION_SPEED)
 }
 
 navbarToggler.addEventListener('click', () => {
-
-  if ( navbar.classList.contains('opening') || navbar.classList.contains('closing') ) { return false } 
-
   navbar.classList.contains('opened') ? closeMobileNavbar() : openMobileNavbar()
 })
